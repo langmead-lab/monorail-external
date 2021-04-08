194b898e21dfef11f382405983aa40ba61921a43
ChristopherWilks <broadsword@gmail.com>
2021-04-08 15:39:21 -0400

    adding retrieval of post-run Unify validation row numbers

--- get_unify_refs.sh
+++ get_unify_refs.sh
@@ -10,13 +10,16 @@ pushd ${org}_unify
 #first grab all the disjoint exon mapping files (mapping back to annotated exons & genes)
 #as well as a stand in file with 0's for those samples w/o any exon sums (blank_exon_sums)
 #finally get the chromosome sizes for the genome reference used in recount-pump
-for f in disjoint2exons.bed.gz disjoint2exons2genes.bed.gz disjoint2exons2genes.rejoin_genes.bed.gz recount_pump.chr_sizes.tsv.gz blank_exon_sums.gz exon_bitmasks.tsv.gz exon_bitmask_coords.tsv.gz; do
+for f in disjoint2exons.bed.gz disjoint2exons2genes.bed.gz disjoint2exons2genes.rejoin_genes.bed.gz recount_pump.chr_sizes.tsv.gz blank_exon_sums.gz exon_bitmasks.tsv.gz exon_bitmask_coords.tsv.gz ; do
     unzipped=$(echo $f | sed 's/\.gz$//')
     if [[ ! -e "$unzipped" ]]; then
         wget https://recount-ref.s3.amazonaws.com/${org}_unify/$f
         gunzip $f
     fi
 done
+
+#get rows counts for Unify post-run validation
+wget https://recount-ref.s3.amazonaws.com/${org}_unify/gene_exon_annotation_row_counts.tsv
 
 #next get list of annotated jx's which is separate the main annotations used in recount-pump
 #annotated junctions stay gzipped
